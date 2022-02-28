70078fb0d95418c551b8868cb77ae0ce32104d71
Karrar <51439971+KarrarAaljaber@users.noreply.github.com>
2022-02-28 13:09:58 +0100

    commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,21 +3,18 @@ const Todo = ({ todo, id, toDoList, setToDoList }) => {
     const completeTodo = () =>{
         
         let copy = [...toDoList];
-            var todo = copy.find(t => t.id === id);
-            console.log(todo?.id)
-        
-            let result = arrayRemove(copy, todo);
-            setToDoList(result);
+        var todo = copy.find(t => t.id === id);
+        console.log(todo?.id)
+    
+        let result =  copy.filter(function(element){ 
+            return element != todo; 
+        });
+        setToDoList(result);
         
      
       }
 
-      const arrayRemove = (arr, value) => { 
-    
-        return arr.filter(function(ele){ 
-            return ele != value; 
-        });
-    }
+      
       
     return (
       <div className="todo">
